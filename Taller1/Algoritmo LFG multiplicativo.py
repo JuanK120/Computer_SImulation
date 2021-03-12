c8a5087dd8a97771568f546ae1e1ea66b09d6f9f
Juan C Rosero <juancamilo.roserolopez@gmail.com>
2021-03-11 22:38:52 -0500

    commit 3

--- Taller1/Algoritmo LFG multiplicativo.py
+++ Taller1/Algoritmo LFG multiplicativo.py
@@ -1,12 +1,12 @@
 import time;
 fib0 = 0;
 fib1 = 1;
-aux3 = str(time.time_ns());
+aux3 = str(time.time_ns()); #toma el tiempo del sistema para qeu la generacion siempresea diferente
 aux1 = (len(aux3)) - 1;
 aux2 = aux1 - 10;
-aux3 = aux3[aux2:aux1];
+aux3 = aux3[aux2:aux1]; # toma los ultimos 10 dijitos del tiempo
 seed = int(aux3);
-aux4 = int(input("cantidad de numeros: "));
+aux4 = int(input("cantidad de numeros: ")); # recibe la cantidad de numerosqeu se quieren generar con la semilla
 cont = 0;
 while cont < aux4:
     aux5 = fib1;
